fec3b51e070bf6a50948c5afdcc63ca85e3ce5da
jest-in <jestin9900767795@gmail.com>
2022-08-18 23:03:15 +0530

    family.js page is updated with dynamic rendering

--- src/family.js
+++ src/family.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+
+import {useNavigate} from 'react-router-dom';
+
 import Arrow from './Assets/Arrow'
 import Icon_Add from './Assets/Icon_Add'
 import Icon_Close from './Assets/Icon_Close'
@@ -7,283 +11,171 @@ import Icon_Menu from './Assets/Icon_Menu'
 import Icon_Search from './Assets/Icon_Search'
 import Logo from './Assets/logo'
 
+import axios from "axios"
+
+const url="http://localhost:5000/api/v1/family";
+
+let id='';
+
 export default function Family() {
+  //For dynamic updation of families data
+  const [families, setFamilies] = useState([]);
+
+  // Family-Name
+  const [familyName,setFamilyName]=useState('');
+
+  // For dynamic updation of individual family details
+  const [family,setFamily]=useState([]);
+
+  useEffect(()=>{
+    //Requesting families data (GET)
+    axios.get(url).then((res) => {
+      setFamilies(res.data.data);
+      setFamilyName(res.data.data[0].familyName);
+      setFamily(res.data.data[0].members);
+      id = res.data.data[0]._id;
+    });
+  },[])
+
+  //function for requesting individual family details
+  function FamilyDetails(id,index){
+    setFamilyName(families[index].familyName);
+    setFamily(families[index].members);
+    id=families[index]._id;
+  }
+
+  // Navigation
+  const navigate = useNavigate();
+
   return (
-    <div className="families-container">
-      <header>
-        <div className="nav-div">
-          <nav>
-            <div className="logo-div">
-              <Logo />
+    <>
+      <div className="families-container">
+        <header>
+          <div className="nav-div">
+            <nav>
+              <div className="logo-div">
+                <Logo />
+              </div>
+              <div className="navigations">
+                <a href="#">Overview</a>
+                <a className="registry-nav" href="#">
+                  Registries
+                  <div className="sub-menu1-div">
+                    <ul>
+                      <li>Family Registry</li>
+                      <li>Engagement Registry</li>
+                      <li>Marriage Registry</li>
+                      <li>Death Reigistry</li>
+                    </ul>
+                  </div>
+                </a>
+                <a href="#">Transactions</a>
+                <a href="#">Pious Associations</a>
+              </div>
+            </nav>
+          </div>
+        </header>
+        <div className="graph-div">{/* graph goes here */}</div>
+        <div className="secondary-nav-div">
+          <div className="secondary-nav-subdiv1">
+            <div className="sub1-head">
+              <div className="sub1-heading">
+                <h1>Families</h1>
+              </div>
+              <div className="families-search-div">
+                <input type="text" name="search-name" placeholder="Search" />
+                <Icon_Search />
+              </div>
+              <div className="filter-div">
+                <Icon_Filter />
+              </div>
             </div>
-            <div className="navigations">
-              <a href="#">Overview</a>
-              <a className="registry-nav" href="#">
-                Registries
-                <div className="sub-menu1-div">
-                  <ul>
-                    <li>Family Registry</li>
-                    <li>Engagement Registry</li>
-                    <li>Marriage Registry</li>
-                    <li>Death Reigistry</li>
-                  </ul>
+            <div className="filter-area">
+              <div className="filters-head">
+                <h1>Has 3 children</h1>
+                <div>
+                  <Icon_Close />
                 </div>
-              </a>
-              <a href="#">Transactions</a>
-              <a href="#">Pious Associations</a>
+              </div>
+              <div className="add-filter">
+                <Icon_Add />
+              </div>
             </div>
-          </nav>
-        </div>
-      </header>
-      <div className="graph-div">{/* graph goes here */}</div>
-      <div className="secondary-nav-div">
-        <div className="secondary-nav-subdiv1">
-          <div className="sub1-head">
-            <div className="sub1-heading">
-              <h1>Families</h1>
-            </div>
-            <div className="families-search-div">
-              <input type="text" name="search-name" placeholder="Search" id />
-              <Icon_Search />
-            </div>
-            <div className="filter-div">
-              <Icon_Filter />
+            <hr />
+            <div className="sub-div-content">
+              {families.map((family, index) => {
+                return (
+                  <div
+                    className="sub-entries"
+                    key={family._id}
+                    onClick={() => FamilyDetails(family._id, index)}
+                  >
+                    <h1>{family.familyName}</h1>
+                  </div>
+                );
+              })}
             </div>
           </div>
-          <div className="filter-area">
-            <div className="filters-head">
-              <h1>Has 3 children</h1>
-              <div>
-                <Icon_Close />
+          <div className="secondary-nav-subdiv2">
+            <div className="sub2-head">
+              <div className="sub2-heading">
+                <h1>{familyName}</h1>
+              </div>
+              <div
+                className="view-detail-div"
+                onClick={() => {
+                  console.log("clicked");
+                  navigate("/family-individual");
+                }}
+              >
+                <h1>View in detail</h1>
+                <Arrow />
               </div>
             </div>
-            <div className="add-filter">
-              <Icon_Add />
-            </div>
-          </div>
-          <hr />
-          <div className="sub-div-content">
-            <div className="sub-entries">
-              <h1>Avan Ramsey</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Aola Simon</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-            <div className="sub-entries">
-              <h1>Bablo Jimenez</h1>
-            </div>
-          </div>
-        </div>
-        <div className="secondary-nav-subdiv2">
-          <div className="sub2-head">
-            <div className="sub2-heading">
-              <h1>Aola Simon</h1>
-            </div>
-            <div className="view-detail-div" onclick="clickListener()">
-              <h1>View in detail</h1>
-              <Arrow />
-            </div>
-          </div>
-          <hr />
-          <div className="sub-div2-content">
-            <div className="sub2-content-head">
-              <div className="sub2-name-div">
-                <h1>Name</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>Date of Birth</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>Phone number</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
-            </div>
-            <div className="sub2-content">
-              <div className="sub2-name-div">
-                <h1>Aola Simon</h1>
-              </div>
-              <div className="sub2-dob-div">
-                <h1>05-04-1985</h1>
-              </div>
-              <div className="sub2-phone-div">
-                <h1>9956425685</h1>
-              </div>
+            <hr />
+            <div className="sub-div2-content">
+              {!family ? (
+                <div className="sub2-content-head">
+                  <div className="sub2-name-div">
+                    <h1>Name</h1>
+                  </div>
+                  <div className="sub2-dob-div">
+                    <h1>Date of Birth</h1>
+                  </div>
+                  <div className="sub2-phone-div">
+                    <h1>Phone number</h1>
+                  </div>
+                </div>
+              ) : (
+                <div className="sub2-content-head">
+                  <div className="sub2-dob-div">
+                    <h1>Nothing</h1>
+                  </div>
+                </div>
+              )}
+              {family.map((person, index) => {
+                const { name, dob, phoneNumber } = person;
+                return (
+                  <div className="sub2-content" key={index}>
+                    <div className="sub2-name-div">
+                      <h1>{name}</h1>
+                    </div>
+                    <div className="sub2-dob-div">
+                      <h1>{dob}</h1>
+                    </div>
+                    <div className="sub2-phone-div">
+                      <h1>{phoneNumber}</h1>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
+
+// created to use while viewing in detail about individual family
+export {id};
