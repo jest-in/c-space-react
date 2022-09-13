a8aee314ffda578f8e1bd684aabf7bb6d3de8859
jest-in <jestin9900767795@gmail.com>
2022-09-13 11:32:58 +0530

    death registries updating

--- src/Registries/death-registry-add.js
+++ src/Registries/death-registry-add.js
@@ -1,184 +1,325 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Icon_Menu from '../Assets/Icon_Menu'
 import Icon_Upload from '../Assets/Icon_Upload'
 import Navigation from '../navigation'
 
 export default function DeathRegistryAdd() {
+  // Error handling variables
+  const [nameError,setNameError]=useState();
+  const [houseNameError,setHouseNameError]=useState();
+  const [ageError,setAgeError] = useState();
+  const [fatherError,setFatherError] = useState();
+  const [motherError,setMotherError] = useState();
+  const [parishError,setParishError] = useState();
+  const [wardError,setWardError] = useState();
+  const [addressError,setAddressError] = useState();
+  const [sicknessError,setSicknessError] = useState();
+  const [confessionError,setCconfessionError] = useState();
+  const [viatiumError,setViatiumError] = useState();
+  const [anointingError,setAnointingError] = useState();
+  const [dodError,setDodError] = useState();
+  const [doBurial,setDoBurial] = useState();
+  const [priest,setPriest] = useState();
+
+  // Inputs handling function
+  function inputHandler(event) {
+    console.log('Input Value:',event.target.value);
+  }
+
+  // Submit button handler
+  function submitButtonHandler(){
+
+  }
   return (
     <div className="container-family">
-  <Navigation/>
-  <div className="title-div">
-    <div className="person-head">
-      <h1>Jackson</h1>
-    </div>
-    <div className="registries-nav-div">
-      <a href="#">Baptism Registry</a>
-      <a href="#">Engagement Registry</a>
-      <a href="#">Marriage Registry</a>
-      <a href="#">Death Registry</a>
-    </div>
-    <div className="menu-div">
-      <Icon_Menu/>
-    </div>
-  </div>
-  <hr />
-  <div className="members-entries-div">
-    <div className="registry-div">
-      <div className="registry-details-heading-div">
-        <div className="name-person-div">
-          <div className="heading-name">Name</div>
-          <div className="person-name">
-            <input type="text" name="Name" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
+      <Navigation />
+      <div className="title-div">
+        <div className="person-head">
+          <h1>Jackson</h1>
         </div>
-        <div className="dob-person-div">
-          <div className="heading-dob">House name</div>
-          <div className="person-dob">
-            <input type="text" name="Name" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
+        <div className="registries-nav-div">
+          <a href="#">Baptism Registry</a>
+          <a href="#">Engagement Registry</a>
+          <a href="#">Marriage Registry</a>
+          <a href="#">Death Registry</a>
         </div>
-        <div className="phone-person-div">
-          <div className="heading-phone">Age</div>
-          <div className="person-phone">
-            <input type="text" name="Name" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="baptism-person-div">
-          <div className="heading-baptism">Father</div>
-          <div className="person-baptism">
-            <input type="text" name="Name" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="marriage-person-div">
-          <div className="heading-marriage">Mother</div>
-          <div className="person-marriage">
-            <input type="text" name="Name" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="death-person-div">
-          <div className="heading-death">Parish</div>
-          <div className="person-death">
-            <input type="text" name="Name" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="death-person-ward-div">
-          <div className="heading-ward">Ward</div>
-          <div className="person-death-ward">
-            <input type="text" name="Name" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="death-person-address-div">
-          <div className="heading-address">Address</div>
-          <div className="person-death-address">
-            <input type="text" name="Name" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="death-person-ward-div">
-          <div className="heading-ward">Sickness</div>
-          <div className="person-death-ward">
-            <input type="text" name="Name" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="death-person-sacraments-div">
-          <div className="heading-sacraments">
-            Sacraments Received
-          </div>
-        </div>
-        <div className="death-person-ward-div-sacraments">
-          <div className="heading-confession">Confession</div>
-          <div className="person-death-ward-anointing">
-            <div className="radio-sacraments-1">
-              <input id="confession-yes" name="confession" type="radio" />
-              <label htmlFor="confession-yes">Yes</label>
-            </div>
-            <div className="radio-sacraments-2">
-              <input id="confession-no" name="confession" type="radio" />
-              <label htmlFor="confession-no">No</label>
-            </div>
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="death-person-ward-div-sacraments">
-          <div className="heading-viatium">Viatium</div>
-          <div className="person-death-ward-anointing">
-            <div className="radio-sacraments-1">
-              <input id="viatium-yes" name="viatium" type="radio" />
-              <label htmlFor="viatium-yes">Yes</label>
-            </div>
-            <div className="radio-sacraments-2">
-              <input id="viatium-no" name="viatium" type="radio" />
-              <label htmlFor="viatium-no">No</label>
-            </div>
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="death-person-ward-div-sacraments anointing-div">
-          <div className="heading-anointing">
-            Anointing of sick
-          </div>
-          <div className="person-death-ward-anointing">
-            <div className="radio-sacraments-1">
-              <input id="anointing-yes" name="anointing" type="radio" />
-              <label htmlFor="anointing-yes">Yes</label>
-            </div>
-            <div className="radio-sacraments-2">
-              <input id="anointing-no" name="anointing" type="radio" />
-              <label htmlFor="anointing-no">No</label>
-            </div>
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="death-person-ward-div">
-          <div className="heading-ward">Date of Death</div>
-          <div className="person-death-ward">
-            <input type="date" name="Name" placeholder="DD-MM-YYYY" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="death-person-ward-div">
-          <div className="heading-ward">Date of Burial</div>
-          <div className="person-death-ward">
-            <input type="date" name="Name" placeholder="DD-MM-YYYY" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
-        </div>
-        <div className="death-person-ward-div">
-          <div className="heading-ward">Parish Priest</div>
-          <div className="person-death-ward">
-            <input type="text" name="Name" />
-            <label className="add-family-error" htmlFor="error">This field is required</label>
-          </div>
+        <div className="menu-div">
+          <Icon_Menu />
         </div>
       </div>
-    </div>
-    <div className="registry-photo-div">
-      <Icon_Upload/>
-      <img
+      <hr />
+      <div className="members-entries-div">
+        <div className="registry-div">
+          <div className="registry-details-heading-div">
+            <div className="name-person-div">
+              <div className="heading-name">Name</div>
+              <div className="person-name">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="text"
+                  name="Name"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="dob-person-div">
+              <div className="heading-dob">House name</div>
+              <div className="person-dob">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="text"
+                  name="Name"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="phone-person-div">
+              <div className="heading-phone">Age</div>
+              <div className="person-phone">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="text"
+                  name="Name"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="baptism-person-div">
+              <div className="heading-baptism">Father</div>
+              <div className="person-baptism">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="text"
+                  name="Name"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="marriage-person-div">
+              <div className="heading-marriage">Mother</div>
+              <div className="person-marriage">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="text"
+                  name="Name"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="death-person-div">
+              <div className="heading-death">Parish</div>
+              <div className="person-death">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="text"
+                  name="Name"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="death-person-ward-div">
+              <div className="heading-ward">Ward</div>
+              <div className="person-death-ward">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="text"
+                  name="Name"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="death-person-address-div">
+              <div className="heading-address">Address</div>
+              <div className="person-death-address">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="text"
+                  name="Name"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="death-person-ward-div">
+              <div className="heading-ward">Sickness</div>
+              <div className="person-death-ward">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="text"
+                  name="sickness"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="death-person-sacraments-div">
+              <div className="heading-sacraments">Sacraments Received</div>
+            </div>
+            <div className="death-person-ward-div-sacraments">
+              <div className="heading-confession">Confession</div>
+              <div className="person-death-ward-anointing">
+                <div className="radio-sacraments-1">
+                  <input
+                    onChange={(event) => inputHandler(event)}
+                    id="confession-yes"
+                    name="confession"
+                    type="radio"
+                    value={true}
+                  />
+                  <label htmlFor="confession-yes">Yes</label>
+                </div>
+                <div className="radio-sacraments-2">
+                  <input
+                    onChange={(event) => inputHandler(event)}
+                    id="confession-no"
+                    name="confession"
+                    type="radio"
+                    value={false}
+                  />
+                  <label htmlFor="confession-no">No</label>
+                </div>
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="death-person-ward-div-sacraments">
+              <div className="heading-viatium">Viatium</div>
+              <div className="person-death-ward-anointing">
+                <div className="radio-sacraments-1">
+                  <input
+                    onChange={(event) => inputHandler(event)}
+                    id="viatium-yes"
+                    name="viaticum"
+                    type="radio"
+                    value={true}
+                  />
+                  <label htmlFor="viatium-yes">Yes</label>
+                </div>
+                <div className="radio-sacraments-2">
+                  <input
+                    onChange={(event) => inputHandler(event)}
+                    id="viatium-no"
+                    name="viaticum"
+                    type="radio"
+                    value={false}
+                  />
+                  <label htmlFor="viatium-no">No</label>
+                </div>
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="death-person-ward-div-sacraments anointing-div">
+              <div className="heading-anointing">Anointing of sick</div>
+              <div className="person-death-ward-anointing">
+                <div className="radio-sacraments-1">
+                  <input
+                    onChange={(event) => inputHandler(event)}
+                    id="anointing-yes"
+                    name="anointing"
+                    type="radio"
+                    value={true}
+                  />
+                  <label htmlFor="anointing-yes">Yes</label>
+                </div>
+                <div className="radio-sacraments-2">
+                  <input
+                    onChange={(event) => inputHandler(event)}
+                    id="anointing-no"
+                    name="anointing"
+                    type="radio"
+                    value={false}
+                  />
+                  <label htmlFor="anointing-no">No</label>
+                </div>
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="death-person-ward-div">
+              <div className="heading-ward">Date of Death</div>
+              <div className="person-death-ward">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="date"
+                  name="dod"
+                  placeholder="DD-MM-YYYY"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="death-person-ward-div">
+              <div className="heading-ward">Date of Burial</div>
+              <div className="person-death-ward">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="date"
+                  name="doburial"
+                  placeholder="DD-MM-YYYY"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+            <div className="death-person-ward-div">
+              <div className="heading-ward">Parish Priest</div>
+              <div className="person-death-ward">
+                <input
+                  onChange={(event) => inputHandler(event)}
+                  type="text"
+                  name="parishPriest"
+                />
+                <label className="add-family-error" htmlFor="error">
+                  This field is required
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="registry-photo-div">
+          <Icon_Upload />
+          <img
             className="marriage-photo"
             src={require("../Assets/marriage.png")}
             alt="marriage pic"
           />
+        </div>
+      </div>
+      <div className="desc-div">
+        <div className="desc-heading">Description/Remarks</div>
+        <div className="desc-content">
+          <input
+            onChange={(event) => inputHandler(event)}
+            type="text"
+            name="remarks"
+          />
+        </div>
+        <div className="submit-btn-div">
+          <button onClick={() => submitButtonHandler()}>Submit</button>
+        </div>
+      </div>
     </div>
-  </div>
-  <div className="desc-div">
-    <div className="desc-heading">Description/Remarks</div>
-    <div className="desc-content">
-      <input type="text" name="Description" />
-    </div>
-    <div className="submit-btn-div">
-      <a href>Submit</a>
-    </div>
-  </div>
-</div>
-
-  )
+  );
 }
--- src/Registries/death-registry-all.js
+++ src/Registries/death-registry-all.js
@@ -1,68 +1,63 @@
 import React from 'react'
+import {useState, useEffect } from 'react';
 import Icon_Filter from '../Assets/Icon_Filter'
 import Icon_Search from '../Assets/Icon_Search'
 import Navigation from '../navigation'
 
 export default function DeathRegistryAll() {
+  // useState for AllDeathRegistry
+  const [allDeathRegistry,setAllDeathRegistry]=useState([]);
+
+  useEffect(()=>{
+    // get request
+  },[])
   return (
     <div className="container-family">
-  <Navigation/>
-  <div className="title-div">
-    <div className="family-master">
-      <h1>Death Registry</h1>
+      <Navigation />
+      <div className="title-div">
+        <div className="family-master">
+          <h1>{allDeathRegistry.length ? "" : "Empty "}Death Registry</h1>
+        </div>
+        <div
+          className={`search-div ${allDeathRegistry.length ? "" : "hidden"}`}
+        >
+          <input type="text" name="search-name" placeholder="Search" />
+          <Icon_Search />
+        </div>
+        <div
+          className={`filter-div ${allDeathRegistry.length ? "" : "hidden"}`}
+        >
+          <Icon_Filter />
+        </div>
+      </div>
+      <hr />
+      <div
+        className={`famili-members-div transactions ${
+          allDeathRegistry.length ? "" : "hidden"
+        }`}
+      >
+        <div className="heading-div">
+          <div className="death-slno">S.No</div>
+          <div className="death-name">Name</div>
+          <div className="death-house-name">House Name</div>
+          <div className="death-age">Age</div>
+          <div className="death-date">Date</div>
+          <div className="death-sickness">Sickness</div>
+        </div>
+        {allDeathRegistry.map((person, index) => {
+          const { name, houseName, age, dod, sickness } = person;
+          <div className="member-details-div" key={index}>
+            <div className="death-slno">{index + 1}</div>
+            <div className="death-name">{name ? name : "-"}</div>
+            <div className="death-house-name">
+              {houseName ? houseName : "-"}
+            </div>
+            <div className="death-age">{age ? age : "-"}</div>
+            <div className="death-date">{dod ? dod.split("T")[0] : "-"}</div>
+            <div className="death-sickness">{sickness ? sickness : "-"}</div>
+          </div>;
+        })}
+      </div>
     </div>
-    <div className="search-div">
-      <input type="text" name="search-name" placeholder="Search" id />
-      <Icon_Search/>
-    </div>
-    <div className="filter-div">
-      <Icon_Filter/>
-    </div>
-  </div>
-  <hr />
-  <div className="famili-members-div transactions">
-    <div className="heading-div">
-      <div className="death-slno">S.No</div>
-      <div className="death-name">Name</div>
-      <div className="death-house-name">House Name</div>
-      <div className="death-age">Age</div>
-      <div className="death-date">Date</div>
-      <div className="death-sickness">Sickness</div>
-    </div>
-    <div className="member-details-div">
-      <div className="death-slno">1</div>
-      <div className="death-name">Andappan</div>
-      <div className="death-house-name">Mannukunnel</div>
-      <div className="death-age">60</div>
-      <div className="death-date">5-12-2021</div>
-      <div className="death-sickness">Cancer</div>
-    </div>
-    <div className="member-details-div">
-      <div className="death-slno">2</div>
-      <div className="death-name">Avarachan</div>
-      <div className="death-house-name">Pattimattam</div>
-      <div className="death-age">63</div>
-      <div className="death-date">5-12-2021</div>
-      <div className="death-sickness">Oldage</div>
-    </div>
-    <div className="member-details-div">
-      <div className="death-slno">3</div>
-      <div className="death-name">Rosa</div>
-      <div className="death-house-name">Moolamattam</div>
-      <div className="death-age">73</div>
-      <div className="death-date">5-12-2021</div>
-      <div className="death-sickness">Oldage</div>
-    </div>
-    <div className="member-details-div">
-      <div className="death-slno">4</div>
-      <div className="death-name">Kundan</div>
-      <div className="death-house-name">Kundady</div>
-      <div className="death-age">81</div>
-      <div className="death-date">5-12-2021</div>
-      <div className="death-sickness">Heart Attack</div>
-    </div>
-  </div>
-</div>
-
-  )
+  );
 }
