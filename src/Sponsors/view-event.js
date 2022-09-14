fc3463f6233abde61d39117593d64c6feb4bcd9e
jest-in <jestin9900767795@gmail.com>
2022-09-14 20:30:34 +0530

    view-event.js updated

--- src/Sponsors/view-event.js
+++ src/Sponsors/view-event.js
@@ -1,49 +1,59 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
 import Navigation from '../navigation'
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function ViewEvent() {
+  const navigate = useNavigate();
+
+  // Status
+  const [status,setStatus]=useState('Loading');
+
+  // offers
+  const [offers, setOffers] = useState([]);
+
+  useEffect(()=>{
+    axios.get(`http://localhost:5000/api/v1/offerings`).then((res) => {
+      if (res.data.status === "success") {
+        if(res.data.data.length){
+          setStatus("Available Events");
+          setOffers(res.data.data);
+        }
+        else setStatus("Events not available");
+      }
+    });
+  },[])
+
   return (
     <div className="container-family">
-  <Navigation/>
-  <div className="announce-title-div">
-    <div className="family-master">
-      <h1>Available Events</h1>
+      <Navigation />
+      <div className="announce-title-div">
+        <div className="family-master">
+          <h1>{status}</h1>
+        </div>
+      </div>
+      <hr />
+      <div className={`famili-members-div transactions ${offers.length?'':'hidden'}`}>
+        <div className="heading-div">
+          <div className="public-spons-slno">S.No</div>
+          <div className="public-spons-date">Date</div>
+          <div className="public-spons-event">Event name</div>
+          <div className="public-spons-amt">Amount</div>
+        </div>
+        {offers.map((event, index) => {
+          const { name, festDate, amount } = event;
+          return (
+            <div className="member-details-div" key={index}>
+              <div className="public-spons-slno">{index}</div>
+              <div className="public-spons-date">{festDate}</div>
+              <div className="public-spons-event">{name}</div>
+              <div className="public-spons-amt">{amount}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  </div>
-  <hr />
-  <div className="famili-members-div transactions">
-    <div className="heading-div">
-      <div className="public-spons-slno">S.No</div>
-      <div className="public-spons-date">Date</div>
-      <div className="public-spons-event">Event name</div>
-      <div className="public-spons-amt">Amount</div>
-    </div>
-    <div className="member-details-div">
-      <div className="public-spons-slno">1</div>
-      <div className="public-spons-date">1-4-2023</div>
-      <div className="public-spons-event">Novena</div>
-      <div className="public-spons-amt">5520</div>
-    </div>
-    <div className="member-details-div">
-      <div className="public-spons-slno">2</div>
-      <div className="public-spons-date">1-4-2023</div>
-      <div className="public-spons-event">Novena</div>
-      <div className="public-spons-amt">5520</div>
-    </div>
-    <div className="member-details-div">
-      <div className="public-spons-slno">3</div>
-      <div className="public-spons-date">1-4-2023</div>
-      <div className="public-spons-event">Novena</div>
-      <div className="public-spons-amt">5520</div>
-    </div>
-    <div className="member-details-div">
-      <div className="public-spons-slno">4</div>
-      <div className="public-spons-date">1-4-2023</div>
-      <div className="public-spons-event">Novena</div>
-      <div className="public-spons-amt">5520</div>
-    </div>
-  </div>
-</div>
-
-  )
+  );
 }
