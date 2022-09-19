1dfa3669beba1a9c20fea8e8a6eb59f877bf54f3
shravyass2003@gmail.com <shravyass2003@gmail.com>
2022-09-20 03:56:21 +0530

    commit

--- src/LandingPage/verify-certificate.js
+++ src/LandingPage/verify-certificate.js
@@ -33,67 +33,46 @@ export default function VerifyCertificate() {
   }
 
   return (
-    <div className="forgot-password-body">
-      <div className="container-forgot-password verify-container">
-        <header>
-          <div className="nav-div">
-            <div className="forgot-logo">
-              <Logo/>
-            </div>
-          </div>
-        </header>
-        <div className="forgot-head-div">
-          <div className="forgot-head">
-            <h1>Verify Certificate</h1>
-          </div>
-          <hr />
-        </div>
-        <div className="verify-content-div">
-          <div className="otp-div verify-token-entry">
-            <div className="otp-forms">
-              <label htmlFor="uid">Enter Token</label>
-              <textarea
-                name="sign"
-                id="token"
-                cols={30}
-                rows={10}
-                onChange={(event)=>{
-                  const {name,value}=event.target;
-                  setInputToken((prev)=>{
-                    let data=prev;
-                    data[name]=value;
-                    return data;
-                  })
-                }}
-                defaultValue={inputToken.sign}
-              />
-              <input onClick={()=>proceedButton()}
-                type="button"
-                defaultValue="Proceed"
-                id="otp-btn-proceed"
-              />
-            </div>
-          </div>
-          <div className="otp-div new-pswrd-div verify-success-entries">
-            <div className="otp-forms password-forms">
-              <label htmlFor="uid">Issued To</label>
-              <input type="text" id="userid" readOnly />
-              <label htmlFor="otp" id="otp-label">
-                Issued On
-              </label>
-              <input type="text" id="otp" readOnly />
-              <label htmlFor="otp" id="otp-label">
-                Issued For
-              </label>
-              <input type="text" id="otp" readOnly />
-            </div>
-          </div>
-        </div>
-        <div className="verify-status-div">
-          <h1 className="verify-valid">Signature Verified</h1>
-          <h1 className="verify-invalid">Invalid Certificate</h1>
-        </div>
+    <div className="container-forgot-password verify-container">
+  <header>
+    <div className="nav-div">
+      <img className="forgot-logo" src="/logo.svg" alt="Logo" />
+    </div>
+  </header>
+  <div className="forgot-head-div">
+    <div className="forgot-head">
+      <h1>Verify Certificate</h1>
+    </div>
+    <hr />
+  </div>
+  <div className="verify-content-div">
+    <div className="otp-div verify-token-entry">
+      <div className="otp-forms">
+        <label htmlFor="uid">Enter Signature</label>
+        <textarea name="token" id="token" cols={60} rows={10} className="certificate-token" defaultValue={""} />
+        <input type="button" defaultValue="Proceed" id="otp-btn-proceed" />
       </div>
     </div>
+    <div className="otp-div verify-token-entry">
+      <div className="otp-forms">
+        <label htmlFor="uid">Description</label>
+        <p className="certificate-verify-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Error obcaecati odit magnam earum, fugit sequi
+          cum veritatis fuga magni quia soluta minima sed
+          illum optio cupiditate hic nulla fugiat natus.
+        </p>
+        <label htmlFor="uid" className="certificate-note">Note</label>
+        <p className="certificate-verify-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Error obcaecati odit magnam earum, fugit sequi
+          cum veritatis fuga magni quia soluta minima sed
+          illum optio cupiditate hic nulla fugiat natus.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
   );
 }
